fca52262401cd2436f812588dbf0f947f0b16c41
kyasevich <kyasevich@t1.ru>
2026-07-01 10:58:31 +0200

    test

--- src/main/java/dz3/Main.java
+++ src/main/java/dz3/Main.java
@@ -2,7 +2,19 @@ package dz3;
 
 public class Main {
     public static void main(String[] args) {
+        MyCustomPool pool = new MyCustomPool(10);
+        for (int i = 0; i < 11; i++) {
+            final int task = i;
+            pool.execute(() -> System.out.println("Выполняется задача - " + task));
+        }
 
+        pool.shutdown();
+
+        try {
+            pool.execute(() -> System.out.println("Эта задача не должна выполниться"));
+        } catch (IllegalStateException e) {
+            System.err.println(">>> Перехвачено ожидаемое исключение: " + e.getMessage());
+        }
 
     }
 }
