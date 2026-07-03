e349696725096fb10e95c116dd58ca96550ea5f9
kyasevich <kyasevich@t1.ru>
2026-07-03 11:28:07 +0200

    Done

--- src/main/java/dz4/entity/User.java
+++ src/main/java/dz4/entity/User.java
@@ -1,14 +1,9 @@
 package dz4.entity;
 
-import jakarta.persistence.Column;
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import org.hibernate.annotations.Audited;
-import org.springframework.data.annotation.Id;
+import jakarta.persistence.*;
 
 @Entity
-@Audited.Table(name = "users")
+@Table(name = "users")
 public class User {
 
     @Id
